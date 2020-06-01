e1e8997c2fc9aae90ad464d60aab2d688c068015
Martin Weber <webermartin@seznam.cz>
2020-06-01 13:05:06 +0200

    edited handling of fetched time entries using luxon

--- src/components/pomodoro/TimeEntries.jsx
+++ src/components/pomodoro/TimeEntries.jsx
@@ -1,52 +1,81 @@
+/* eslint-disable consistent-return */
 import React, { useEffect, useState, useContext } from 'react'
 import styled from 'styled-components/macro'
+import { DateTime } from 'luxon'
 
 import { db } from '../../utils/firebase'
 import { AuthContext } from '../../context/authContext'
 
 const TimeEntries = () => {
-  const [entries, setEntries] = useState()
+  const [entries, setEntries] = useState([])
   const { currentUser } = useContext(AuthContext)
 
   useEffect(() => {
     if (currentUser) {
       const { uid } = currentUser
-      const fetchData = async () => {
-        try {
-          await db
-            .doc(`users/${uid}/pomo/timeEntries`)
-            .onSnapshot((snapshot) => {
-              setEntries(snapshot.data().timeEntries)
-            })
-        } catch (err) {
-          console.log(err)
-        }
-      }
-      fetchData()
+      return db.doc(`users/${uid}/pomo/timeEntries`).onSnapshot((snapshot) => {
+        const entriesData = snapshot.data().timeEntries
+        entriesData.map((entry) => {
+          const formattedStartTime = DateTime.fromMillis(
+            +entry.startTime
+          ).toLocaleString(DateTime.TIME_24_SIMPLE)
+          const formattedEndTime = DateTime.fromMillis(
+            +entry.endTime
+          ).toLocaleString(DateTime.TIME_24_SIMPLE)
+
+          return setEntries((prev) => [
+            ...prev,
+            {
+              formattedStartTime,
+              formattedEndTime,
+              duration: entry.durationInMinutes,
+            },
+          ])
+        })
+      })
     }
   }, [currentUser])
 
   return (
     <Wrapper>
-      {entries &&
-        entries.map((entry) => (
-          <>
-            <span>
-              Start : {new Date(entry.startTime).toLocaleTimeString()}
-            </span>
-            <span>End : {new Date(entry.endTime).toLocaleTimeString()}</span>
-            <span>Duration : {entry.durationInMinutes} minutes</span>
-          </>
-        ))}
+      <StyledList>
+        <span>Started</span>
+        <span>Ended</span>
+        <span>Length</span>
+        {entries &&
+          entries.map((entry) => (
+            <TimeEntry key={entry.formattedStartTime}>
+              <span>{entry.formattedStartTime}</span>
+              <span> {entry.formattedEndTime}</span>
+              <span> {entry.duration} minutes</span>
+            </TimeEntry>
+          ))}
+      </StyledList>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
   color: ${(props) => props.theme.colors.dark};
   background: ${(props) => props.theme.colors.white};
+  border-radius: 5px;
+  padding: 0.5em;
+`
+
+const StyledList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: 1fr;
+`
+
+const TimeEntry = styled.li`
+  grid-column: 1/-1;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  span:nth-child(1) {
+    grid-column: 1/2;
+  }
 `
 
 export default TimeEntries
